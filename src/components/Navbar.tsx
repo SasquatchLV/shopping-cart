40e1f9ef453097720cb0b1bbdc1b09ce451559a6
Almighty Sasquatch <almightysasquatch@gmail.com>
2022-06-22 03:38:36 +0300

    Shopping cart with react, bootstrap and context

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,61 @@
+import { Button, Container, Nav, Navbar as NavBarBs } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+import { useShoppingCart } from "../context/ShoppingCartContext"
+
+export function Navbar() {
+  const { openCart, cartQuantity } = useShoppingCart()
+  return (
+    <NavBarBs sticky="top" className="bg-white shadow-sm mb-3 p-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        {cartQuantity > 0 && (
+          <Button
+            onClick={() => openCart()}
+            style={{ width: "3rem", height: "3em", position: "relative" }}
+            variant="outline-primary"
+            className="rounded-circle"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
+            <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                color: "white",
+                width: "1.5rem",
+                height: "1.5rem",
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                transform: "translate(25%, 25%)",
+              }}
+            >
+              {cartQuantity}
+            </div>
+          </Button>
+        )}
+      </Container>
+    </NavBarBs>
+  )
+}
